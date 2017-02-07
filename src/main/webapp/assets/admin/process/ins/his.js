f744b23f4d496db3d4ff94336824fc2aea5aeea3
tylanbin <735461331@qq.com>
2017-02-07 14:56:19 +0800

    预留正在运行和执行结束的流程的页面

---
+++ src/main/webapp/assets/admin/process/ins/his.js
@@ -0,0 +1,81 @@
+$(function() {
+	$('#dg-list').datagrid({
+		fit : true,
+		striped : true,
+		border : true,
+		idField : 'id',
+		rownumbers : true,
+		fitColumns : true,
+		singleSelect : false,
+		pagination : true,
+		pageSize : 15,
+		pageList : [10, 15, 20],
+		url : 'admin/process/ins/data/his',
+		queryParams : {},
+		method : 'get',
+		frozenColumns : [[{
+					field : 'ck',
+					checkbox : true
+				}]],
+		columns : [[{
+					"field" : "id",
+					"title" : "编号"
+				}, {
+					"field" : "key",
+					"title" : "KEY"
+				}, {
+					"field" : "name",
+					"title" : "名称"
+				}, {
+					"field" : "version",
+					"title" : "版本"
+				}, {
+					"field" : "category",
+					"hidden" : true
+				}, {
+					"field" : "deploymentId",
+					"hidden" : true
+				}, {
+					"field" : "description",
+					"title" : "描述",
+					formatter : function(value, row, index) {
+						if (value) {
+							if (value.length > 10) {
+								return value.substring(0, 10) + '...';
+							} else {
+								return value;
+							}
+						}
+					}
+				}, {
+					"field" : "isSuspended",
+					"title" : "状态",
+					formatter : function(value, row, index) {
+						if (value) {
+							return '挂起';
+						} else {
+							return '激活';
+						}
+					}
+				}]],
+		loadMsg : '数据载入中...'
+	});
+	$('#dg-list').datagrid('getPager').pagination({
+		beforePageText : '第',
+		afterPageText : '页    共 {pages} 页',
+		displayMsg : '当前显示 {from} - {to} 条记录    共 {total} 条记录'
+	});
+});
+
+function search(value, name) {
+	$('#dg-list').datagrid('clearSelections');
+	$('#dg-list').datagrid('reload', {
+		params : '{ "' + name + '" : "' + value + '" }'
+	});
+}
+
+function func_reload() {
+	$('#searchbox').searchbox('setValue', '');
+	$('#dg-list').datagrid('clearSelections');
+	$('#dg-list').datagrid('reload', {});
+}
